216c7a7595e44398efb0f9c9b201701ad6ef9ab3
Massimiliano Cannarozzo <maxcanna@gmail.com>
2016-08-04 21:27:10 +0200

    Use correct repo name

--- release.sh
+++ release.sh
@@ -17,11 +17,12 @@ fi
 npm version minor || exit 1
 
 # Generate changelog from pull requests
-github-changes -o request -r request \
+github-changes -o maxcanna \
+  -r node-lightify-rest \
   --auth --verbose \
   --file CHANGELOG.md \
   --only-pulls --use-commit-body \
-  --date-format '(YYYY/MM/DD)' \
+  --date-format '- YYYY-MM-DD' \
   || exit 1
 
 # Since the tag for the new version hasn't been pushed yet, any changes in it
@@ -30,7 +31,6 @@ version="$(grep '"version"' package.json | cut -d'"' -f4)"
 sed -i -e "s/^### upcoming/## [$version]/" CHANGELOG.md
 
 # This may fail if no changelog updates
-# TODO: would this ever actually happen?  handle it better?
 git add CHANGELOG.md; git commit -m 'Update changelog'
 
 # Publish the new version to npm
